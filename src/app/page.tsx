2a984630fdacba740afee560b4548600998e14b1
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2025-03-15 23:44:19 -0600

    Add "use client" directive to page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { AppBar, Toolbar, Typography, Container, CssBaseline, ThemeProvider, createTheme, Button, Grid, Card, CardContent, Avatar } from "@mui/material";
 
